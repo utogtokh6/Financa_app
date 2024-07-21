6e8ae0748c6ee697c37511d56be93be7713c8be7
tog <96715682+utogtokh6@users.noreply.github.com>
2024-07-21 10:29:40 +0800

    controller-uudiig nemj  keybord event oruulav

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-document.querySelector(".add__btn").addEventListener('click', function () { alert("clicked add__btn") })
+//Дэлгэцтэй ажиллах контроллер
+var uiController = (function () { })();
+
+//Санхүүтэй ажиллах контроллер
+var financeController = (function () { })();
+
+//Прогрхмын холбогч контроллер
+var appController = (function (uiController, financeController) {
+
+    var ctrlAddItem = function () {
+        // 1. оруулах өгөгдлийг дэлгэцээс олж авна
+        console.log("Дэлгэцээс өгөгдлөө авах хэсэг")
+        // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална
+        // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана.
+        // 4. Төсвийг тооцоолно.
+        // 5. Эцсийн үлдэгдэл тооцоог дэлгэцэнд гаргана.
+    };
+
+    document.querySelector(".add__btn").addEventListener('click', function () {
+        alert("clicked add__btn");
+        ctrlAddItem();
+
+
+    });
+
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            ctrlAddItem();
+        }
+    });
+
+
+
+
+})(uiController, financeController);
