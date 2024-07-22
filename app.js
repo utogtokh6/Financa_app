de7affa08af6acdc3f8da4035b8f756fb4f96ad0
tog <96715682+utogtokh6@users.noreply.github.com>
2024-07-22 20:57:19 +0800

    orlogo zarlagiig x tovchoor ustgadag bolgono

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var uiController = (function () {
         tusuvLabel: ".budget__value",
         incomeLabel: ".budget__income--value",
         expenseLabel: ".budget__expenses--value",
-        percentageLabel: ".budget__expenses--percentage"
+        percentageLabel: ".budget__expenses--percentage",
+        containerDiv: ".container"
 
     };
     return {
@@ -54,18 +55,25 @@ var uiController = (function () {
 
         },
 
+        deleteListItem: function(id){
+
+            var el = document.getElementById(id);
+            el.parentNode.removeChild(el);
+
+        },
+
         addListItem: function(item, type){
             // Orlogo zarlagiin elementiig aguulsan html-iig beltgene.
             var html, list;
             if (type === 'inc'){
                 list = DOMsrings.incomeList;
-                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%DESCRIPTION%</div><div class="right clearfix"><div class="item__value">%VALUE%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+                html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%DESCRIPTION%</div><div class="right clearfix"><div class="item__value">%VALUE%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
             } else {
                 list = DOMsrings.expenseList
-                html ='<div class="item clearfix" id="expense-%id%"><div class="item__description">%DESCRIPTION%</div><div class="right clearfix"><div class="item__value">%VALUE%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+                html ='<div class="item clearfix" id="exp-%id%"><div class="item__description">%DESCRIPTION%</div><div class="right clearfix"><div class="item__value">%VALUE%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
             }
 
-            console.log(list);
+            // console.log(list);
             //Ter HTML dotroo zarlagiin utguudiig REPLACE ashiglaj uurchilj ugnu.
             html = html.replace('%id%', item.id);
             html = html.replace('%DESCRIPTION%', item.description);
@@ -132,9 +140,28 @@ var financeController = (function () {
         }
         },
 
+
+        deleteItem: function(type, id){
+            // data-s zuvhun object-uudiin ID-g avaad massivd hiine.
+            var ids = data.items[type].map(function(el){
+                return el.id;
+            });
+            // console.log('ids: ' + ids);
+            var index = ids.indexOf(id);
+
+            // console.log('index: ' + index);
+        // indexOf function ni massiv-d bhgui utgiig ugvul -1 gesen ugta butsaadag tul -1-s busad tohioldold ustgana
+        if(index !== -1){
+
+            // console.log('index: ' + index + ' deer bgaa elementiig ustgah gej bna');
+            data.items[type].splice(index,1)
+        }
+
+        },
+
         addItem: function(type, desc, val){
             var item, id;
-            console.log(data);
+            // console.log(data);
             if(data.items[type].length === 0) {
                 id = 1;
             } else {
@@ -193,6 +220,33 @@ var appController = (function (uiController, financeController) {
                 ctrlAddItem();
             }
         });
+        document.querySelector(DOM.containerDiv).addEventListener('click', function(event){
+            //ustgah x tovchiin parentNode^5 deer ID bgaag gargaj avch huvisagchid ugch bna...
+            var id = event.target.parentNode.parentNode.parentNode.parentNode.id;
+            // console.log(id);
+
+            if(id){
+               // gargaj avsan inc-1, exp-3 geh formattai id-g split-r salgaj avna
+                var arr = id.split('-');
+                var type = arr[0];
+                var itemId = parseInt(arr[1]);
+
+                            // console.log(type + ' ===> ' + itemId)
+
+            // 1. Sankhuugiin module-s type, id ashiglaad ustgana.
+            financeController.deleteItem(type, itemId)
+            // 2. Delgets deerees ene elementiig ustgana
+
+            uiController.deleteListItem(id);
+
+            // 3. Uldegdel tootsoog shinechilj haruulna6
+            }
+
+
+
+
+        });
+
     };
 
     return {
